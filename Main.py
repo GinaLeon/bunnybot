b36574602b551448807446eced2e2fadea4a9511
GinaLeon <xxginaleonxx@gmail.com>
2022-12-04 19:26:48 -0500

    Update Main.py

--- Main.py
+++ Main.py
@@ -2,19 +2,25 @@ import nextcord
 from nextcord import Interaction
 from nextcord.ext import commands
 
+
+
 intents = nextcord.Intents.default()
 intents.message_content = True
 intents.members = True
 
-client = commands.Bot(command_prefix = '>', intents= intents)
+client = commands.Bot(command_prefix = '/', intents= intents)
 
 @client.event
 async def on_ready():
     print("The bot is here")
 
 testingSERVERID = 800097892353179658
-@nextcord.slash_command(name = "hello", description="Replies with hello", guild_ids=[testingSERVERID])
-async def hellocommand(interaction: Interaction):
-    await interaction.response.send_message("Hello")
+@client.slash_command(description="Replies with hello", guild_ids=[testingSERVERID])
+async def hello(interaction: nextcord.Interaction):
+    print("running hello: command")
+    print("interaction object")
+    print(interaction)
+    await interaction.send("sup", ephemeral=True)
 
-client.run('MTA0ODM4NzQ1NzEyNjc2MDQ4OA.Gn4h6_.gW2-2SXmGtDqI8f3mTlB0GavZsuMrXuCS95d1Q')
+
+client.run('MTA0ODM4NzQ1NzEyNjc2MDQ4OA.G8oTSz.T95qXf2g4OhcKhWTbn5AR1Jz4F2ERpw4Bzc064')
